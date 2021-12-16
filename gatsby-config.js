eec6039ecdc4e21a52a559c18f9ca1f7969506ef
finn2212 <stolle.finn@googlemail.com>
2021-12-16 17:36:44 +0800

    blog works

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}blog`,
+        path: `${__dirname}/blog`,
         name: `markdown-pages`,
       },
     },
